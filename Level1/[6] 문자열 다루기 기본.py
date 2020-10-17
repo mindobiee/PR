8dd8e69fb64a13d5dd0eee0041649eb916124e23
mindobiee <mk1795@naver.com>
2020-10-17 21:57:52 +0900

    파일 이름 번호 매기기

--- Level1/[6] 문자열 다루기 기본.py
+++ Level1/[6] 문자열 다루기 기본.py
@@ -1,6 +1,9 @@
 def solution(s):
+    # 문자열의 길이가 4나 6일 때
     if len(s)!=4 and len(s)!=6:
         return False
+    # 숫자로만 구성되어 있는지 확인
+    # 알파벳이 있으면 거짓을 리턴
     for i in s:
         if i.isalpha():
             return False
