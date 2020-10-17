ab064347e263f6afb7c8ff49656775f0a170c499
mindobiee <mk1795@naver.com>
2020-10-17 21:38:28 +0900

    k번째 수.py 주석 수정

--- Level1/k번째 수.py
+++ Level1/k번째 수.py
@@ -1,20 +1,19 @@
 
 def solution(array, commands):
     answer = []
-    # 2차원 배열인 commands에 대해 열의 길이만큼 반복해주어
-    # 배열을 하나씩 처리해준다.
+    # 2차원 배열인 commands에 대해 열의 길이만큼 반복해주어 배열을 하나씩 처리해준다.
     for i in range(len(commands)):
-        list= commands[i]
-        start = list[0]
-        end = list[1]
-        order = list[2]
+        list = commands[i]
+        start, end, order = list[0], list[1], list[2]
+
         # 리스트의 슬라이싱 기법을 이용해서 한번에 배열을 만들어준다.
-        # 슬라이싱배열 [첫번째 인덱스:마지막 인덱스+1] 이므로
-        # start 번째부터 end 번째까지의 원소를 가지는 배열을 만들고 싶으면 아래와 같이 작성한다.
+        # 슬라이싱배열 [첫번째 인덱스:마지막 인덱스+1] 이므로 start~ end 번째 인덱스를 가지는 배열은 다음과 같다.
         temp = array[start-1:end]
+
         # 배열을 오름차순으로 정렬한다.
         temp.sort()
-        # list가 0번부터 시작하기 때문에 구하고자하는 인덱스 값에 -1해준다.
+
+        # 정렬한 temp 배열에 order 번째 인덱스 원소를 answer 리스트에 저장한다.
         answer.append(temp[order-1])
 
     return answer
