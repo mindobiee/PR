b38653f8347863939232458ab1177861f449dc15
mindobiee <mk1795@naver.com>
2020-10-17 21:28:17 +0900

    김서방찾기.py 주석달기

--- Level1/김서방찾기.py
+++ Level1/김서방찾기.py
@@ -1,6 +1,8 @@
 def solution(seoul):
+    # 반복문을 이용하여 "Kim"이 있는 인덱스를 저장한다.
     for i in range(len(seoul)):
         if seoul[i]=="Kim":
             location=i
+    # 정답 문자열을 만들어 준다.
     answer="김서방은 "+str(location)+"에 있다"
     return answer
