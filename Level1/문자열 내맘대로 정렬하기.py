05ae93eff98db892eaa684f62785abce7690ab1d
mindobiee <mk1795@naver.com>
2020-10-17 21:32:01 +0900

    문자열 내맘대로 정렬하기.py 주석달기

--- Level1/문자열 내맘대로 정렬하기.py
+++ Level1/문자열 내맘대로 정렬하기.py
@@ -1,11 +1,14 @@
 def solution(strings, n):
     array=[]
     result=[]
+    # 각 word의 n번째 원소와 word를 새 array에 저장한다. (word[n],word)
     for word in strings:
         array.append((word[n],word))
-    # 첫번째 원소인 인덱스 n을 기준으로 sort된다.
+
+    # array에 저장된 첫번째 원소(word[n])를 기준으로 sort된다. a.sort()
     array.sort()
-    # word 만 저장해주기
+
+    # result는 array에서 두번째 원소인 (word)만 저장해주기
     for i in array:
         result.append(i[1])
     return result
