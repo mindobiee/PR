b0e10ffdf928104dbbc62abb95147664d864d67e
mindobiee <mk179536@gmail.com>
2020-11-23 16:34:43 +0900

    그래프 탐색(DFS/BFS)- L2. 타겟넘버 (주석추가)

--- 유형별 문제/그래프탐색(DFS,BFS)/[1] 타겟넘버.py
+++ 유형별 문제/그래프탐색(DFS,BFS)/[1] 타겟넘버.py
@@ -2,6 +2,7 @@
 # dfs 사용하기
 answer=0
 
+
 # dfs 함수
 def dfs(numbers, target, sum, index):
     global answer
@@ -9,13 +10,14 @@ def dfs(numbers, target, sum, index):
         if sum==target:
             answer=answer+1
         return
+    # 재귀적이니 용법을 사용한다. (+)일때, (-)일때
     dfs(numbers,target,sum+numbers[index],index+1) # 하나는 +
     dfs(numbers,target,sum-numbers[index],index+1) # 하나는 -
 
 
 def solution(numbers, target):
-    global answer
-    dfs(numbers, target,0,0)
+    global answer # 전역변수 사용하기 위한 global 키워드
+    dfs(numbers, target,0,0) # 0번 인덱스부터 시작
     return answer
 
 
