0fa54926d5fcc7b4f660229567f5e11d4e6e6359
mindobiee <mk179536@gmail.com>
2020-11-26 00:17:58 +0900

    그리디 - Lv2. 구명보트 (출력제거) ok

--- 유형별 문제/그리디/[2] 구명보트.py
+++ 유형별 문제/그리디/[2] 구명보트.py
@@ -1,7 +1,5 @@
-# 앞에서부터 더하기, limit가 넘을 때에는 새로운 보트에 추가하기 (안넘을 때엔 그 전 보트에 추가하기)
-# 필요 : people 의 무게!
-# 두 명 한정됨!!!!
-# 접근법; 무거운 사람 + 가벼운 사람 -> 만약 limit보다 넘으면 계속 줄여나가기 (!)
+# 접근법; "가장" 무거운 사람 + "가장" 가벼운 사람 -> 만약 limit보다 넘으면 계속 줄여나가기
+
 from collections import deque
 
 
@@ -11,23 +9,20 @@ def solution(people, limit):
     queue = deque(sorted(people)) # 오름차순으로 정렬
     lightest = queue.popleft()  # 가벼운 사람
 
-    while lightest: # 먼저 pop한 lightest를 위해서
-        print(queue)
-        if not queue: # 큐가 더이상 존재하지 않으면,
+    while lightest:
+        if not queue: # 큐가 (비교대상이) 더이상 존재하지 x
             alone+=1
             break
         heaviest = queue.pop()  # 무거운 사람
-        sum = lightest+heaviest # 두 사람의 무게
+        sum = lightest+heaviest # 비교할 대상 : 두 사람의 무게 합
         if sum <= limit: # 같이 탈 조건 : limit보다 적을 때 같이 타기
             together+=1
-            if not queue:
+            if not queue: # empty pop - 오류 방지
                 break
             else:
                 lightest = queue.popleft()
         else: # 무거운 사람 혼자 보트 타기
             alone+=1
-    print(alone)
-    print(together)
     answer=alone+together
     return answer
 
