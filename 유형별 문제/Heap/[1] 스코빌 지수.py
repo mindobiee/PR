a0d6ed2cdba0312871685bad3fbb65e7470d01a4
mindobiee <mk179536@gmail.com>
2020-11-26 15:02:55 +0900

    Heap - Lv2. 스코빌 지수 ok (heapq 사용)

---
+++ 유형별 문제/Heap/[1] 스코빌 지수.py
@@ -0,0 +1,51 @@
+# 가장 작은 두 개의 지수->새로운 지수 + 원래 지수
+# 1: test ok, but 런타임에러, 실패, 시간초과
+
+
+from collections import deque
+
+
+def solution1(scoville, K):
+    answer = 0
+    scoville = deque(sorted(scoville))
+    while scoville:
+        new = scoville.popleft() + scoville.popleft() * 2
+        scoville.appendleft(new)
+        answer += 1
+        if min(scoville) >= K:
+            break
+        if not scoville:
+            return -1
+    return answer
+
+
+
+# 2. Heapq 이용하기
+# ok!! with everything (정확성, 효율성)
+
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)  # 리스트 -> 힙큐로 바꿔준다 :):)
+
+    while scoville:
+        answer += 1
+
+        new = heapq.heappop(scoville)  # 가장 작은 값이 꺼내진다.(힙의 이름 명시하는 것 잊지 말기)
+        if not scoville:
+            return -1
+        new += heapq.heappop(scoville) * 2
+        heapq.heappush(scoville, new)
+        smallest = heapq.heappop(scoville)
+        if smallest >= K:
+            break
+        else:
+            heapq.heappush(scoville, smallest)
+
+    return answer
+
+
+
+print(solution(scoville=[1, 2, 3, 9, 10, 12], K=7))
